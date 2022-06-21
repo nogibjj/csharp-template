0da121a3d4c087124b0dab01a6131afaa00bf2d6
Noah Gift <noah.gift@gmail.com>
2022-06-21 19:24:14 +0000

    adding unit tests

---
+++ HelloTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace MyFirstUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
