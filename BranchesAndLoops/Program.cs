0b89628380e15ccdbae06dbad99b054fe566c257
Noah Gift <noah.gift@gmail.com>
2022-06-28 19:23:15 +0000

    adding loops

---
+++ BranchesAndLoops/Program.cs
@@ -0,0 +1,46 @@
+﻿//ExploreIf();
+
+void ExploreIf()
+{
+    int a = 5;
+    int b = 3;
+    if (a + b > 10)
+    {
+        Console.WriteLine("The answer is greater than 10");
+    }
+    else
+    {
+        Console.WriteLine("The answer is not greater than 10");
+    }
+
+    int c = 4;
+    if ((a + b + c > 10) && (a > b))
+    {
+        Console.WriteLine("The answer is greater than 10");
+        Console.WriteLine("And the first number is greater than the second");
+    }
+    else
+    {
+        Console.WriteLine("The answer is not greater than 10");
+        Console.WriteLine("Or the first number is not greater than the second");
+    }
+
+    if ((a + b + c > 10) || (a > b))
+    {
+        Console.WriteLine("The answer is greater than 10");
+        Console.WriteLine("Or the first number is greater than the second");
+    }
+    else
+    {
+        Console.WriteLine("The answer is not greater than 10");
+        Console.WriteLine("And the first number is not greater than the second");
+    }
+}
+
+int counter = 0;
+while (counter < 10)
+{
+    Console.WriteLine($"Hello World! The counter is {counter}");
+    counter++;
+    Console.WriteLine($"I updated the counter! Now the counter is {counter}");
+}
